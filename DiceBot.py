4824d5cb2896a2d4ac597c1c108a5e3402ef1940
DaBotYdk <44483476+DaBotYdk@users.noreply.github.com>
2018-11-30 15:34:19 -0700

    Update DiceBot.py

--- DiceBot.py
+++ DiceBot.py
@@ -4,14 +4,9 @@ from discord.ext import commands
 import asyncio
 import random
 
-swears = ['fuck','shit','bitch','dick','ass','tit']
-
-pfnt = ["Watch your profanity, ","Don't swear on my Christian Discord server, "]
-
 Client = discord.Client()
 
 client = commands.Bot(command_prefix = "$")
-
 
 @client.event
 async def on_ready():
@@ -19,10 +14,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    for swr in swears:
-        if swr in message.content.lower():
-            return
-            #await client.send_message(message.channel, (random.choice(pfnt) + str(message.author) + '!'))
+    dcrl = message.content()
+    if dcrl[0, 6] == "DiceBot":
+        if isinstance(dcrl[8, len(dcrl)-1], int):
+            maxnum = dcrl[8, len(dcrl)-1]
+            await client.send_message(message.channel, random.randint(1, maxnum))
 
-client.run("NTA0ODM3NDM1MDEyOTM5Nzc2.Dre9rg.-Lf_3dmDQZfLvmcy5SAJvDlOt4I")
-#NTA0ODM3NDM1MDEyOTM5Nzc2.DraJGg.GL52r8mFz6YF88t5mbbqG8v40gw
+client.run("TOKEN")
